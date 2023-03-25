32fbbfcb9e8a14c78f9dec27c793974de4780332
Luong Minh The Vinh <luongminhthevinh1606@outlook.com>
2023-03-25 13:47:12 +0700

    #3 login

--- src/main/java/GUI/Server/LoginGUI.java
+++ src/main/java/GUI/Server/LoginGUI.java
@@ -1,9 +1,21 @@
 package GUI.Server;
 
+import GUI.Components.SideBar.ImagePanel;
+import Utils.Helper;
+import model.Account;
+import service.AccountService;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.URL;
 
 public class LoginGUI {
+    private AccountService accountService;
     public LoginGUI() {
         JFrame jFrame = new JFrame();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -11,181 +23,159 @@ public class LoginGUI {
         // set vị trí cho khung đăng nhập
         // start
         int width = 400;
-        int height = 400;
+        int height = 300;
         int x = (screenSize.width - width) / 2;
         int y = (screenSize.height - height) / 2;
 
         jFrame.setSize(screenSize.width, screenSize.height);
 
-        JPanel backgroundPanel = new JPanel();
-//        backgroundPanel.setPreferredSize(new Dimension(screenSize.width, screenSize.height));
-        backgroundPanel.setLayout(new BorderLayout());
+        ImagePanel backgroundPanel = new ImagePanel();
+        backgroundPanel.setImage(getImage("https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/06/Riot-Games-Xbox-Game-Pass.jpg",-1,-1).getImage());
+
+//       JPanel backgroundPanel = new JPanel();
+
+        var layout = new FlowLayout();
+        layout.setAlignment(FlowLayout.CENTER);
+        backgroundPanel.setLayout(layout);
 
         int widthPageStartPanel = screenSize.width;
         int heightPageStartPanel = y;
         JPanel pageStartPanel = new JPanel();
-        pageStartPanel.setBackground(new Color(0,0,0,16));
+        pageStartPanel.setBackground(new Color(0,0,0,0));
         pageStartPanel.setPreferredSize(new Dimension(widthPageStartPanel, heightPageStartPanel));
-        backgroundPanel.add(pageStartPanel, BorderLayout.PAGE_START);
-
-        int widthLineStarPanel = x;
-        int heightLineStarPanel = height;
-        JPanel lineStartPanel = new JPanel();
-//        lineStartPanel.setBackground(Color.gray);
-        lineStartPanel.setPreferredSize(new Dimension(widthLineStarPanel, heightLineStarPanel));
-        backgroundPanel.add(lineStartPanel, BorderLayout.LINE_START);
-        // end
+        backgroundPanel.add(pageStartPanel);
 
         // login
         // start
-        JPanel loginPanel = new JPanel();
-        loginPanel.setPreferredSize(new Dimension(width, height));
-        loginPanel.setLayout(new FlowLayout());
-        backgroundPanel.add(loginPanel, BorderLayout.CENTER);
 
-        //Đăng nhập
+        var loginLayout = new FlowLayout(FlowLayout.LEFT);
+        JPanel loginPanel = new JPanel();
+        loginPanel.setPreferredSize(new Dimension(width,height));
+        loginPanel.setLayout(loginLayout);
+        loginPanel.setBackground(new Color(255,255,255,255));
+
+        // Đăng Nhập
         // start
-        JPanel logoPanel = new JPanel();
-        logoPanel.setLayout(new BorderLayout());
-        JLabel logoLabel = new JLabel("Đăng Nhập");
-        JPanel logoLeftPanel = new JPanel();
-        logoLeftPanel.setPreferredSize(new Dimension(400, 20));
-        logoPanel.add(logoLeftPanel, BorderLayout.LINE_START);
-        logoLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
-        logoLabel.setPreferredSize(new Dimension(500, 30));
-        logoPanel.add(logoLabel, BorderLayout.CENTER);
-//        logoPanel.setBackground(Color.gray);
-        loginPanel.add(logoPanel);
+        JLabel logoLoginLabel = new JLabel("Đăng Nhập");
+        logoLoginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        logoLoginLabel.setFont(new Font("Times New Roman",Font.BOLD,30));
+        logoLoginLabel.setPreferredSize(new Dimension(width-30,40));
+        loginPanel.add(logoLoginLabel);
         // end
 
         // Đăng nhập để truy cập vào chức năng của máy chủ
-        // start
-        JPanel statusPanel = new JPanel(new BorderLayout());
-
         JLabel statusLabel = new JLabel("Đăng nhập để truy cập vào chức năng của máy chủ");
-        statusLabel.setFont(new Font("Times New Roman", Font.ITALIC, 14));
-//        JPanel statusNorthPanel = new JPanel();
-//        statusNorthPanel.setPreferredSize(new Dimension(0,0));
-//        statusPanel.add(statusNorthPanel, BorderLayout.PAGE_START);
-        JPanel statusEastPanel = new JPanel();
-        statusEastPanel.setPreferredSize(new Dimension(100, 20));
-        statusPanel.add(statusEastPanel, BorderLayout.LINE_START);
-        statusLabel.setPreferredSize(new Dimension(width, 20));
-        statusPanel.add(statusLabel, BorderLayout.CENTER);
-//        statusPanel.setBackground(Color.gray);
-        loginPanel.add(statusPanel);
-        // end
+        statusLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        statusLabel.setFont(new Font("Times New Roman",Font.ITALIC,12));
+        statusLabel.setPreferredSize(new Dimension(width-30,20));
+        loginPanel.add(statusLabel);
 
         // username
         // start
-        JPanel usernamePanel = new JPanel(new BorderLayout());
-
-        JPanel usernamePanel1 = new JPanel();
-        JPanel pageStart = new JPanel();
-//        pageStart.setBackground(Color.gray);
-        usernamePanel1.add(pageStart, BorderLayout.PAGE_START);
-        JPanel east = new JPanel();
-        east.setPreferredSize(new Dimension(50, 20));
-        east.setBackground(Color.BLACK);
-//        usernamePanel1.setBackground(Color.gray);
-        usernamePanel1.add(east, BorderLayout.LINE_START);
-        usernamePanel1.setLayout(new BorderLayout());
         JLabel usernameLabel = new JLabel("Username");
-        usernamePanel1.add(usernameLabel, BorderLayout.CENTER);
-        usernameLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
-        usernamePanel1.setPreferredSize(new Dimension(width, 20));
-        usernamePanel.add(usernamePanel1, BorderLayout.PAGE_START);
+        usernameLabel.setFont(new Font("Times New Roman",Font.BOLD,18));
+        usernameLabel.setBorder(new EmptyBorder(20,25,5,5));
+        usernameLabel.setPreferredSize(new Dimension(width-30,40));
+        loginPanel.add(usernameLabel);
+        // end
 
+        // txtUsername
+        // start
+        FlowLayout centerLayout = new FlowLayout();
+        centerLayout.setAlignment(FlowLayout.CENTER);
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.setLayout(centerLayout);
+        usernamePanel.setPreferredSize(new Dimension(width-10,35));
+        usernamePanel.setBackground(new Color(255,255,255,255));
 
-        JTextField usernameTextField = new JTextField();
-        usernameTextField.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-        usernameTextField.setPreferredSize(new Dimension(350, 18));
-        usernamePanel.add(usernameTextField, BorderLayout.CENTER);
-
-        JPanel jPanelEastUS = new JPanel();
-        jPanelEastUS.setPreferredSize(new Dimension(25, 20));
-//        jPanelEastUS.setBackground(Color.gray);
-        usernamePanel.add(jPanelEastUS, BorderLayout.LINE_START);
-
-        JPanel jPanelWestUS = new JPanel();
-        jPanelWestUS.setPreferredSize(new Dimension(25, 20));
-//        jPanelWestUS.setBackground(Color.gray);
-        usernamePanel.add(jPanelWestUS, BorderLayout.LINE_END);
-        usernamePanel.setPreferredSize(new Dimension(width, 45));
+        JTextField txtUsername = new JTextField();
+        txtUsername.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        txtUsername.setBorder(new EmptyBorder(0,5,0,0));
+        txtUsername.setBackground(new Color(255,255,255,255));
+        txtUsername.setPreferredSize(new Dimension(width-50,20));
+        usernamePanel.add(txtUsername);
         loginPanel.add(usernamePanel);
         // end
 
         // password
         // start
-        JPanel passwordPanel = new JPanel(new BorderLayout());
         JLabel passwordLabel = new JLabel("Password");
-        passwordLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
-        passwordPanel.add(passwordLabel, BorderLayout.PAGE_START);
-        passwordPanel.add(passwordLabel, BorderLayout.PAGE_START);
+        passwordLabel.setFont(new Font("Times New Roman",Font.BOLD,18));
+        passwordLabel.setBorder(new EmptyBorder(5,25,5,5));
+        passwordLabel.setPreferredSize(new Dimension(width-30,20));
+        loginPanel.add(passwordLabel);
+        //end
 
-        JPanel panelEastPW = new JPanel();
-        panelEastPW.setPreferredSize(new Dimension(25, 20));
-//        panelEastPW.setBackground(Color.gray);
-        passwordPanel.add(panelEastPW, BorderLayout.LINE_START);
+        // txtPassword
+        // start
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.setLayout(centerLayout);
+        passwordPanel.setPreferredSize(new Dimension(width-10,35));
+        passwordPanel.setBackground(new Color(255,255,255,255));
 
-        JPasswordField jPasswordField = new JPasswordField();
-        jPasswordField.setPreferredSize(new Dimension(300, 20));
-        passwordPanel.add(jPasswordField, BorderLayout.CENTER);
-
-        JPanel panelWestPW = new JPanel();
-        panelWestPW.setPreferredSize(new Dimension(25, 20));
-//        panelWestPW.setBackground(Color.gray);
-        passwordPanel.add(panelWestPW, BorderLayout.LINE_END);
-
-        passwordPanel.setPreferredSize(new Dimension(width, 45));
+        JPasswordField txtPassword = new JPasswordField();
+        txtPassword.setBorder(new EmptyBorder(0,5,0,0));
+        txtPassword.setPreferredSize(new Dimension(width-50,20));
+        txtPassword.setBackground(new Color(255,255,255,255));
+        passwordPanel.add(txtPassword);
         loginPanel.add(passwordPanel);
         // end
 
         // button
         // start
-        JPanel panelButton = new JPanel();
-        JButton jButton = new JButton("Đăng Nhập");
-        jButton.setSize(40, 60);
-        panelButton.add(jButton);
-        panelButton.setPreferredSize(new Dimension(width, 60));
-        loginPanel.add(panelButton);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(centerLayout);
+        buttonPanel.setPreferredSize(new Dimension(width-10,70));
+        buttonPanel.setBackground(new Color(255,255,255,255));
+        JButton button = new JButton("Đăng Nhập");
+        button.setSize(40,60);
+//        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setHorizontalAlignment(SwingConstants.CENTER);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                var username = txtUsername.getText();
+                var password = txtPassword.getPassword();
+//                var user = accountService.login(username,password.toString());
+                Account user = null;
+                if (user == null) {
+                    var result = "Tài Khoản đăng nhập hoặc Mật Khẩu của bạn không đúng, vui lòng nhập lại";
+                    JOptionPane.showMessageDialog(null,result,null,JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    MainUI.getInstance();
+                }
+            }
+        });
+        buttonPanel.add(button);
+        loginPanel.add(buttonPanel);
         // end
 
-
+        backgroundPanel.add(loginPanel);
         // end
-
-
-        int widthLineEndPanel = screenSize.width - widthLineStarPanel - width;
-        int heightLineEndPanel = height;
-        JPanel lineEndPanel = new JPanel();
-//        lineEndPanel.setBackground(Color.gray);
-        lineEndPanel.setPreferredSize(new Dimension(widthLineEndPanel, heightLineEndPanel));
-        backgroundPanel.add(lineEndPanel, BorderLayout.LINE_END);
-
-        int withPageEndPanel = screenSize.width;
-        int heightPagePanel = y;
-        JPanel pageEndPanel = new JPanel();
-        pageEndPanel.setPreferredSize(new Dimension(widthLineEndPanel, heightPagePanel));
-//        pageEndPanel.setBackground(Color.gray);
-//        pageEndPanel.setPreferredSize(new Dimension(100,500));
-        backgroundPanel.add(pageEndPanel, BorderLayout.PAGE_END);
-
         jFrame.add(backgroundPanel);
 //        jFrame.pack();
         jFrame.setUndecorated(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
-//        Connection a =null;
-//        try {
-//            var b = a.createStatement();
-//            b.execute("select * from Accoutn;")
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
+    }
+
+    public static ImageIcon getImage(String path, int width, int height) {
+        try {
+            Image image = ImageIO.read(new URL(path));
+//            Image image = ImageIO.read(new File(path));
+            if (width == -1 || height == -1) {
+                return new ImageIcon(image);
+            }
+            Image image1 = image.getScaledInstance(width,height,0);
+            return new ImageIcon(image1);
+        }catch (Exception e){
+            return null;
+        }
     }
 
     public static void main(String[] args) {
+        Helper.initUI();
         new LoginGUI();
     }
-
 }
