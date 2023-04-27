54597a9b6a4f23748310ee63ba580ac85d404c14
Mirai3103 <huuhoag1412@gmail.com>
2023-04-27 16:48:45 +0700

    minify

--- src/main/java/GUI/Server/Order/Cart.java
+++ src/main/java/GUI/Server/Order/Cart.java
@@ -97,9 +97,16 @@ public class Cart extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jTable2.setToolTipText("");
